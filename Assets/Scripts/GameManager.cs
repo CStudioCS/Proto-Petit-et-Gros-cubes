6cbee88b16e92c676d383c407d9467c07b0bcc34
Nicolas Dupuis <nicolas.dupuis@student-cs.fr>
2026-09-23 21:09:30 +0200

    mouvements gérés par un couple

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -56,7 +56,9 @@ public class GameManager : MonoBehaviour
     public void Rejouer()
     {
         Time.timeScale = 1f;
-        
+
+        partieTerminee = false;
+
         movementJ1.respawn();
         movementJ2.respawn();
 
